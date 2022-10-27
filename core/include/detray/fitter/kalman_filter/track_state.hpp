72aacfbc9ab6de8052663d625fce461f9507654d
beomki-yeo <beomki.yeo@gmail.com>
2022-10-27 11:04:05 -0700

    backup

--- core/include/detray/fitter/kalman_filter/track_state.hpp
+++ core/include/detray/fitter/kalman_filter/track_state.hpp
@@ -7,12 +7,27 @@
 
 #pragma once
 
+// Project include(s).
+#include "detray/tracks/bound_track_parameters.hpp"
+
 namespace detray {
 
+/// Track state definition for kalman filtering
 template <typename algebra_t>
 struct track_state{
 
+    DETRAY_HOST_DEVICE 
+    inline bound_track_parameters<algebra_t> filtered() const {
+        return m_filtered;
+    }
 
+    DETRAY_HOST_DEVICE 
+    inline bound_track_parameters<algebra_t> smoothed() const {
+        return m_smoothed;
+    }
+
+    bound_track_parameters<algebra_t> m_filtered;
+    bound_track_parameters<algebra_t> m_smoothed;
 };
 
 }  // namespace detray
