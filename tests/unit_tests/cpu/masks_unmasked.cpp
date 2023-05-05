9ff561f9bcedc9e0c2419fa617764bab2d8aaab4
Guilherme <g.almeida@cern.ch>
2023-05-05 14:22:35 +0200

    fix unused variables

--- tests/unit_tests/cpu/masks_unmasked.cpp
+++ tests/unit_tests/cpu/masks_unmasked.cpp
@@ -14,8 +14,6 @@
 using namespace detray;
 using point3_t = test::point3;
 
-constexpr scalar tol{1e-7f};
-
 /// This tests the basic functionality of an unmasked plane
 GTEST_TEST(detray_core, unmasked) {
     point3_t p2 = {0.5f, -9.f, 0.f};
